6b8841975839bced237b702235a3ab1ef8583dba
Alexander Quiñones <alex@Alex.local>
2017-07-27 16:57:41 -0400

    update js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,56 @@
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var wins = 0;
+var losses = 0;
+var computerGuess = computerChoices[Math.floor(Math.random() * 26)];
+console.log(computerGuess);
+var guesses = 9;
+
+
+
+document.onkeyup = function(keyPressed) {
+
+  	var keyPressed = keyPressed,
+    charCode = keyPressed.keyCode,
+    lettersGuessed = String.fromCharCode(charCode);
+
+	var userGuess = event.key;
+
+	if (userGuess == computerGuess) {
+		wins++;
+		console.log("You won a game!");
+		alert("You won a game!")
+		computerGuess = computerChoices[Math.floor(Math.random() * 26)]
+		console.log(computerGuess);
+		document.getElementById("lettersGuessedID").innerHTML = "";
+	}
+
+	else {
+		guesses--;
+		document.getElementById("lettersGuessedID").innerHTML += lettersGuessed;
+
+		}
+
+	if (guesses == 0) {
+		losses++;
+		guesses=9;
+		console.log("You lost a game.")
+		alert("You lost a game.")
+		computerGuess = computerChoices[Math.floor(Math.random() * 26)];
+		console.log(computerGuess);
+		document.getElementById("lettersGuessedID").innerHTML = "";
+	}
+
+
+var html = 
+	"<p>wins: " + wins + "</p>" +
+	"<p>losses: " + losses + "</p>" +
+	"<p>You have " + guesses + " guesses left.</p>";
+
+	document.querySelector ("#game").innerHTML = html;
+
+	}
+
+function resetGame() {
+    location.reload();
+}
